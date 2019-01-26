f5c3cec6f3cd539edce073a19d07fcf9ae3022b7
Wade Roberts <waderoberts123@gmail.com>
2019-01-25 20:24:13 -0700

    Updates to testing

--- tests.py
+++ tests.py
@@ -21,27 +21,38 @@ class TestCompressNetcdf(unittest.TestCase):
         compress_netcfd(folder_path, start_date, out_path, "Qout_south_america_continental", 10)
 
         ds = xr.open_dataset(os.path.join(os.getcwd(), out_path, start_date + ".nc"))
+
         flow_array = ds["Qout"].data
         flow_array_high_res = ds["Qout_high_res"].data
-        inititilization_values = ds["initialization_values"].data
+        initialization_values = ds["initialization_values"].data
         date = ds["date"].data
         date_high_res = ds["date_high_res"].data
-        ensemble_number = ds["ensemble_number"].data
+        rivids = ds["rivid"].data
 
-        # TODO: Create files containing the values that should be in the arrays, double check to make sure it is correct
         benchmark_flow_array = np.load("Test_files/Comparison_Files/benchmark_flow_array.npy")
-        # benchmark_array_high_res = np.zeros((10, 10))
-        # benchmark_initialization_values = np.zeros((10))
+        benchmark_array_high_res = np.load("Test_files/Comparison_Files/benchmark_array_high_res.npy")
+        benchmark_initialization_values = np.array([1.9156765937805176, 1.7540310621261597, 1.5276174545288086,
+                                                    2.0937132835388184, 3.4932050704956055, 3.678524971008301,
+                                                    0.854943037033081, 1.688418984413147, 13.452759742736816,
+                                                    3.7613487243652344], dtype=np.float32)
         benchmark_date = np.array(['2019-01-05', '2019-01-06', '2019-01-07', '2019-01-08',
                                    '2019-01-09', '2019-01-10', '2019-01-11', '2019-01-12',
                                    '2019-01-13', '2019-01-14', '2019-01-15', '2019-01-16',
                                    '2019-01-17', '2019-01-18', '2019-01-19'],
                                   dtype='datetime64[ns]')
+        benchmark_date_high_res = np.array(['2019-01-05', '2019-01-06', '2019-01-07', '2019-01-08',
+                                            '2019-01-09', '2019-01-10', '2019-01-11', '2019-01-12',
+                                            '2019-01-13', '2019-01-14'], dtype='datetime64[ns]')
+        benchmark_rivids = np.array([192474, 192473, 192470, 192469, 192454, 192452, 192448, 192446, 192451, 192450],
+                                    dtype=np.int32)
 
         print("Testing")
         self.assertTrue(np.all(np.isclose(flow_array, benchmark_flow_array)))
-
+        self.assertTrue(np.all(np.isclose(flow_array_high_res, benchmark_array_high_res)))
+        self.assertTrue(np.all(np.isclose(initialization_values, benchmark_initialization_values)))
         self.assertTrue(np.all(date == benchmark_date))
+        self.assertTrue(np.all(date_high_res == benchmark_date_high_res))
+        self.assertTrue(np.all(rivids == benchmark_rivids))
 
         ds.close()
 
---
+++ validate_forecasts.py
@@ -0,0 +1,247 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import os
+import numba as nb
+import time
+import dask.array as da
+
+
+def compute_all(work_dir, memory_to_allocate_gb, date_strings):
+    """
+    Parameters
+    ----------
+
+    work_dir: str
+        Folder containing the NetCDF files generated from the "compress_netcdf.py" script. The files must be names as
+        YYYYMMDD.nc for this script to work.
+
+    memory_to_allocate_gb: float
+        The memory to allocate when performing calculations, care should be taken to not allocate to much, as this could
+        decrease performace significantly if the computer has to store data in swap memory.
+
+    date_strings: list of str
+        List of the date strings in the time series.
+
+    """
+
+    # Configuring chunk size based on the allocated memory
+    array_size_bytes = 3060  # Based on 15 x 51 member array
+    memory_to_allocate_bytes = memory_to_allocate_gb * 1e9
+    chunk_size = int(np.floor(memory_to_allocate_bytes / ((array_size_bytes * len(date_strings)) + len(date_strings))))
+
+    print("Chunk Size:", chunk_size)
+
+    # Creating a list of files to get data from
+    files = [os.path.join(work_dir, i + ".nc") for i in date_strings]
+
+    # Initializing lists to store dask arrays from all of the NetCDF Files
+    list_of_dask_q_arrays = []
+    list_of_dask_init_arrays = []
+
+    # Creating a large dask array with all of the data in it
+    start = time.time()
+    for file in files:
+        ds = xr.open_dataset(file, chunks={"rivid": chunk_size})
+
+        tmp_dask_q_array = ds["Qout"].data
+        list_of_dask_q_arrays.append(tmp_dask_q_array)
+
+        tmp_dask_init_array = ds["initialization_values"].data
+        list_of_dask_init_arrays.append(tmp_dask_init_array)
+
+        ds.close()
+    end = time.time()
+
+    # Stacking individual arrays to form a large array
+    big_dask_q_array = da.stack(list_of_dask_q_arrays)
+    big_dask_init_array = da.stack(list_of_dask_init_arrays)
+
+    print("Time to create dask arrays: ", end - start)
+
+    # Retrieving the number of streams and their corresponding Rivids
+    tmp_dataset = xr.open_dataset(files[0])
+
+    num_of_streams = tmp_dataset['rivid'].size
+    rivids = tmp_dataset['rivid'].data
+
+    tmp_dataset.close()
+
+    # Initializing values for the main loop
+    num_chunk_iterations = int(np.ceil(num_of_streams / chunk_size))
+    start_chunk = 0
+    end_chunk = chunk_size
+    list_of_tuples_with_metrics = []
+
+    # Main loop to calculate metrics for the data in memory, then move on to next chunk
+    for chunk_number in range(num_chunk_iterations):
+
+        start = time.time()
+        big_forecast_data_array = np.asarray(big_dask_q_array[:, start_chunk:end_chunk, :, :])
+        big_init_data_array = np.asarray(big_dask_init_array[:, start_chunk:end_chunk])
+        end = time.time()
+        print("Time to read from disk:", end - start)
+
+        rivids_chunk = rivids[start_chunk:end_chunk]
+
+        start = time.time()
+        results_array = numba_calculate_metrics(
+            big_forecast_data_array, big_init_data_array, len(files), big_forecast_data_array.shape[1], 15
+        )
+        end = time.time()
+        print("Numba Calculation Time: ", end - start)
+
+        for rivid in range(results_array.shape[1]):
+            for forecast_day in range(results_array.shape[0]):
+                tmp_array = results_array[forecast_day, rivid, :]
+                tuple_to_append = (rivids_chunk[rivid], '{} Day Forecast'.format(forecast_day + 1),
+                                   tmp_array[0], tmp_array[1], tmp_array[2])
+                list_of_tuples_with_metrics.append(tuple_to_append)
+
+        start_chunk += chunk_size
+        end_chunk += chunk_size
+
+    # Saving the results of the validation in CSV format
+    final_df = pd.DataFrame(list_of_tuples_with_metrics,
+                            columns=['Rivid', 'Forecast Day', 'CRPS', 'CRPS BENCH', 'CRPSS'])
+    final_df.to_csv(r'/Users/wade/PycharmProjects/Forecast_Validation/South_America_Test_DF.csv', index=False)
+
+
+@nb.njit(parallel=True)
+def numba_calculate_metrics(forecast_array, initialization_array, number_of_start_dates, number_of_streams,
+                            num_forecast_days):
+    """
+    Parameters
+    ----------
+
+    forecast_array: 4D ndarray
+        A 4 dimensional numPy array with the following dimensions: 1) Start Date Number (365 if there are a year's
+        forecasts), 2) Unique stream ID, 3) Forecast Days (e.g. 1-15 in a 15 day forecast), 4) Ensembles
+
+    initialization_array: 2D ndarray
+        A 2 dimenensional NumPy array with the following dimensions: 1) Start Dates, 2) Unique stream ID
+
+    number_of_start_dates: The number of start dates for the analysis to perform
+
+    number_of_streams:
+        The number of streams in the analysis
+
+    num_forecast_days:
+        The number of forecast days in the analysis
+
+    Returns
+    -------
+    ndarray
+        An ndarray with the folowing dimenstions:
+        1) Forecast Day: 1-15 in the case of a 15 day forecast
+        2) Rivid: The stream unique ID
+        3) Metrics: CRPS, CRPS_BENCH, CRPSS
+
+    """
+    return_array = np.zeros((num_forecast_days, number_of_streams, 3), dtype=np.float32)
+
+    for stream in nb.prange(number_of_streams):
+        for forecast_day in range(num_forecast_days):
+            initialization_vals = initialization_array[(forecast_day + 1):, stream]
+            # np.savetxt("init_test.txt", initialization_vals)
+            forecasts = forecast_array[:(number_of_start_dates - (forecast_day + 1)), stream, forecast_day, :]
+            # np.savetxt("forecasts_test.txt", forecasts)
+            benchmark_forecasts = initialization_array[:(number_of_start_dates - (forecast_day + 1)), stream]
+            # np.savetxt("benchmark_forecasts_test.txt", benchmark_forecasts)
+
+            crps = ens_crps(initialization_vals, forecasts)
+            crps_bench = mae(initialization_vals, benchmark_forecasts)
+            if crps_bench == 0:
+                crpss = np.inf
+                print("Warning: Division by zero on: ", stream)
+            else:
+                crpss = 1 - crps / crps_bench
+
+            return_array[forecast_day, stream, 0] = crps
+            return_array[forecast_day, stream, 1] = crps_bench
+            return_array[forecast_day, stream, 2] = crpss
+
+            # print(crps, crps_bench, crpss)
+        if (stream % 1000) == 0:
+            print("Count: ", stream)
+
+    return return_array
+
+
+@nb.njit()
+def mae(sim, obs):
+    return np.mean(np.abs(sim - obs))
+
+
+@nb.njit()
+def ens_crps(obs, fcst_ens, adj=np.nan):
+
+    rows = obs.size
+    cols = fcst_ens.shape[1]
+
+    col_len_array = np.ones(rows) * cols
+    sad_ens_half = np.zeros(rows)
+    sad_obs = np.zeros(rows)
+    crps = np.zeros(rows)
+
+    crps = numba_crps(
+        fcst_ens, obs, rows, cols, col_len_array, sad_ens_half, sad_obs, crps, np.float64(adj)
+    )
+
+    # Calc mean crps as simple mean across crps[i]
+    crps_mean = np.mean(crps)
+
+    return crps_mean
+
+
+@nb.njit()
+def numba_crps(ens, obs, rows, cols, col_len_array, sad_ens_half, sad_obs, crps, adj):
+    for i in range(rows):
+        the_obs = obs[i]
+        the_ens = ens[i, :]
+        the_ens = np.sort(the_ens)
+        sum_xj = 0.
+        sum_jxj = 0.
+
+        j = 0
+        while j < cols:
+            sad_obs[i] += np.abs(the_ens[j] - the_obs)
+            sum_xj += the_ens[j]
+            sum_jxj += (j + 1) * the_ens[j]
+            j += 1
+
+        sad_ens_half[i] = 2.0 * sum_jxj - (col_len_array[i] + 1) * sum_xj
+
+    if np.isnan(adj):
+        for i in range(rows):
+            crps[i] = sad_obs[i] / col_len_array[i] - sad_ens_half[i] / \
+                      (col_len_array[i] * col_len_array[i])
+    elif adj > 1:
+        for i in range(rows):
+            crps[i] = sad_obs[i] / col_len_array[i] - sad_ens_half[i] / \
+                      (col_len_array[i] * (col_len_array[i] - 1)) * (1 - 1 / adj)
+    elif adj == 1:
+        for i in range(rows):
+            crps[i] = sad_obs[i] / col_len_array[i]
+    else:
+        for i in range(rows):
+            crps[i] = np.nan
+
+    return crps
+
+
+if __name__ == "__main__":
+
+    starting_date = "2018-08-19"
+    ending_date = "2019-01-13"
+
+    dates_range = pd.date_range(starting_date, ending_date)
+    dates_strings = dates_range.strftime("%Y%m%d").tolist()
+
+    workspace = r'/home/wade/Remote/global_streamflow_backup/reforecasts/South_America'
+    MEMORY_TO_ALLOCATE = 1  # GB
+
+    start = time.time()
+    compute_all(workspace, MEMORY_TO_ALLOCATE, dates_strings)
+    end = time.time()
+    print(end - start)
